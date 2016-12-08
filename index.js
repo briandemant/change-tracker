5122b4e91363a2e73925b0bec1af73d6f8d59c62
Priyank Chodisetti <priyank@coursera.org>
2016-12-07 17:40:12 -0800

    Cleanup loglines

--- index.js
+++ index.js
@@ -5,14 +5,14 @@ const router = express.Router({ strict: true }); // eslint-disable-line new-cap
 const request = require('request');
 
 module.exports = (config) => {
-  console.dir(config);
   const buildJenkins = (crumb, epicBotText) => {
+    console.log(`Triggering jenkins build with botText: ${epicBotText}`)
     const urlOptions = {
       url: `https://${config.jenkins.userId}:${config.jenkins.userApiToken}@jenkins.dkandu.me/view/web/job/web-courselenium/buildWithParameters?token=${config.jenkins.projectToken}&VERSION=master&cause=${epicBotText}`,
       crumb: crumb
     }
     request.get(urlOptions, function (error, response, body) {
-      console.dir(body);
+      console.log('Request to jenkins finished');
     });
   }
 
@@ -33,7 +33,6 @@ module.exports = (config) => {
       };
       request.get(crumbUrlOptions, function (error, response, body) {
         if (!error && response.statusCode == 200) {
-          console.log(body);
           buildJenkins(JSON.parse(body).crumb, epicBotText);
         }
       });
